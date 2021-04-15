0645d325d268243514e2608e5a32cfdbde1a2983
DESKTOP-9OBBPQS\JoeyEspinosa <joeyespinosa28@gmail.com>
2021-04-15 14:42:58 +0800

    Update on login check

--- app/src/main/java/com/udacity/locationreminderapp/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/locationreminderapp/authentication/AuthenticationActivity.kt
@@ -4,32 +4,67 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.locationreminderapp.R
+import com.udacity.locationreminderapp.databinding.ActivityAuthenticationBinding
 import com.udacity.locationreminderapp.locationreminders.RemindersActivity
-import kotlinx.android.synthetic.main.activity_authentication.*
-
 
 class AuthenticationActivity : AppCompatActivity() {
 
-    companion object {
-        const val TAG = "AuthenticationActivity"
-        const val SIGN_IN_RESULT_CODE = 1001
-    }
+    private lateinit var binding: ActivityAuthenticationBinding
+    private val viewModel by viewModels<AuthenticationViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
 
-        button.setOnClickListener {
-            signIn()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+
+        observeAuthenticationState()
+        binding.button.setOnClickListener {
+            launchSignInFlow()
         }
-
     }
 
-    private fun signIn() {
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                Log.i(TAG, "Signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    private fun observeAuthenticationState() {
+        viewModel.authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                AuthenticationViewModel.AuthenticationState.AUTHENTICATED -> {
+                    navigateToRemindersActivity()
+                }
+                else -> {
+                    binding.button.setOnClickListener {
+                        launchSignInFlow()
+                    }
+                }
+            }
+        })
+    }
+
+    private fun navigateToRemindersActivity() {
+        startActivity(Intent(this, RemindersActivity::class.java))
+        finish()
+    }
+
+    private fun launchSignInFlow() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
@@ -45,20 +80,9 @@ class AuthenticationActivity : AppCompatActivity() {
         )
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SIGN_IN_RESULT_CODE) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                startReminderActivity()
-            } else {
-                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
-            }
-        }
+    companion object {
+        const val TAG = "AuthenticationActivity"
+        const val SIGN_IN_RESULT_CODE = 1001
     }
 
-    private fun startReminderActivity() {
-        val intent = Intent(this, RemindersActivity::class.java)
-        startActivity(intent)
-    }
 }
